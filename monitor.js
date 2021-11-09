170824c50c25b293e142a37ff5defedb46551bc7
Paul Khlopkov <mainguy@mail.ru>
2021-11-10 02:08:03 +0300

    Update and rename app.js to monitor.js

--- monitor.js
+++ monitor.js
@@ -6,25 +6,15 @@ const port = 20345
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('X-Powered-By', 'ISUpNGN/0.1a');
-  function format(seconds){
-    function pad(s){
-      return (s < 10 ? '0' : '') + s;
-    }
-    var hours = Math.floor(seconds / (60*60));
-    var minutes = Math.floor(seconds % (60*60) / 60);
-    var seconds = Math.floor(seconds % 60);
-    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
-  }
   var uptime = process.uptime();
   res.end(
-
       JSON.stringify({
         uptime: {
           days: Math.floor(os.uptime() / 86400),
-          hours: Math.floor((os.uptime() - 86400) / (60*60)),
+          hours: Math.trunc((Math.floor(os.uptime() / 60 / 60 / 24) - Math.floor(os.uptime() / (60*60)) / 24) * -24),
           minutes: Math.floor(os.uptime() % (60*60) / 60),
           seconds: Math.floor(os.uptime() % 60),
-          total_seconds: os.uptime()
+          total_seconds: Math.trunc(os.uptime())
         },
         powered_by: {
           platform: 'ISUpNGN/0.1a',
