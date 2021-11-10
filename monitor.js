0e839d3bd8a8930df9ea15c244039272dc2fe09e
Paul Khlopkov <mainguy@mail.ru>
2021-11-10 14:50:38 +0300

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -4,28 +4,31 @@ const os = require('os')
 const port = 20345
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-Powered-By', 'ISUpNGN/0.1a');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'ISUpNGN/0.1a');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-  var uptime = process.uptime();
-  res.end(
-      JSON.stringify({
-        uptime: {
-          days: Math.floor(os.uptime() / 86400),
-          hours: Math.trunc((Math.floor(os.uptime() / 60 / 60 / 24) - Math.floor(os.uptime() / (60*60)) / 24) * -24),
-          minutes: Math.floor(os.uptime() % (60*60) / 60),
-          seconds: Math.floor(os.uptime() % 60),
-          total_seconds: Math.trunc(os.uptime())
-        },
-        powered_by: {
-          platform: 'ISUpNGN/0.1a',
-          author: 'IceSlam',
-          website: 'https://iceslam.ru',
-          git: 'https://github.com/iceslam/ISUpNGN'
-        }
-      })
-  );
+    var uptime = process.uptime();
+    res.end(
+        JSON.stringify({
+            server_time: new Date(),
+            uptime: {total_uptime: Math.floor(os.uptime() / 86400) + ' дней ' + Math.trunc((Math.floor(os.uptime() / 60 / 60 / 24) - Math.floor(os.uptime() / (60*60)) / 24) * -24) + ' часов ' + Math.floor(os.uptime() % (60*60) / 60) + ' минут ' + Math.floor(os.uptime() % 60) + ' секунд',
+                days: Math.floor(os.uptime() / 86400),
+                hours: Math.trunc((Math.floor(os.uptime() / 60 / 60 / 24) - Math.floor(os.uptime() / (60*60)) / 24) * -24),
+                minutes: Math.floor(os.uptime() % (60*60) / 60),
+                seconds: Math.floor(os.uptime() % 60),
+                total_seconds: Math.trunc(os.uptime())
+            },
+            powered_by: {
+                platform: 'ISUpNGN/0.1a',
+                author: 'IceSlam',
+                website: 'https://iceslam.ru',
+                git: 'https://github.com/iceslam/ISUpNGN'
+            }
+        })
+    );
 })
 
-app.listen(process.env.APP_PORT || port, process.env.APP_IP || 'localhost', () => {})
+app.listen(20345, '0.0.0.0', () => {
+    console.log('Uptime monitor ISUpNGN started on 0.0.0.0:20345')
+})
